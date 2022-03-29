24a337a12efcd3df31352b5b9064a128300c8486
lucia <ruadaslucia@gmail.com>
2022-03-29 09:45:10 +0200

    done function randomNum

--- js/main.js
+++ js/main.js
@@ -6,10 +6,35 @@ const select = document.querySelector('.js-selectBox');
 //caja intro apuesta
 const imputNum = document.querySelector('.js-inputNum');
 //boton jugar
-const btn = document.querySelector('.js-button');
+const button = document.querySelector('.js-button');
 //texto cambiante
 const playText = document.querySelector('.js-goPlay');
 //texto saldo restante
 const leftover = document.querySelector('.js-moneyLeft');
 
 
+
+const randomNum = getRandomNumber (100);
+let cont = 0;
+//----funciones
+
+function getRandomNumber (max){
+    return Math.ceil(Math.random()*max);
+} //funciona
+console.log("random number: " + randomNum);
+
+//evento click en boton
+button.addEventListener("click", handleClickButton);
+
+function handleClickButton(){
+    //comprobar el numero
+    checkNum();
+    //con cada click aumenta el contador
+    sumCont();
+}
+
+//checkNum();
+
+
+
+//sumcont();
