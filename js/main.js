a0b46947af560cae0048459bcb8c228080bcf79e
lucia <ruadaslucia@gmail.com>
2022-03-29 09:33:38 +0200

    declared const

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 "use strict";
+//declarar constantes que me hace falta traer de html
+
+//caja de seleccion
+const select = document.querySelector('.js-selectBox');
+//caja intro apuesta
+const imputNum = document.querySelector('.js-inputNum');
+//boton jugar
+const btn = document.querySelector('.js-button');
+//texto cambiante
+const playText = document.querySelector('.js-goPlay');
+//texto saldo restante
+const leftover = document.querySelector('.js-moneyLeft');
+
+
