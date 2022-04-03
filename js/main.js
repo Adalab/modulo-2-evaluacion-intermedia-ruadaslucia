6440ba75169bd80fbaa959503f188034cc0a28ef
lucia <ruadaslucia@gmail.com>
2022-04-03 19:03:42 +0200

    start again

--- js/main.js
+++ js/main.js
@@ -1,56 +1,19 @@
 "use strict";
-//---------------------constantes
+const selectNumber=document.querySelector('.js-selectBox');
+const bet=document.querySelector('.js-input');
+const playButton=document.querySelector('.jsplayButton');
+const text=document.querySelector('.js-goPlay');
+const initialMoney=document.querySelector('.js-money');
 
-const selectNum = document.querySelector('.js-selectBox');
-const inputNum = document.querySelector('.js-inputNum');
-const button = document.querySelector('.js-button');
-const playText = document.querySelector('.js-goPlay');
-const leftover = document.querySelector('.js-moneyLeft');
+const winText= "Has ganado el doble de lo apostado :)";
+const loseText="Has perdido lo apostado :(";
 
+const maxNumber=6;
 
-//const para los mensajes
-const winningMessage = "¡Has ganado el doble de lo apostado";
-const losingMessage = "Has perdido lo apostado:(";
-
-
-
-const maxNum = 6;
-const randomNumber = getRandomNumber(maxNum); 
-
-//---------------------funciones
-
-//ranmdom number
-function getRandomNumber (max){
-    return Math.ceil(Math.random()*max);
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
 }
-//evento click en los botones
 button.addEventListener("click", handleClickButton);
-
-//funcion manejadora
 function handleClickButton(){
-    let selectedNumber = parseInt(selectNum.value);
-    let money = parseInt(inputNum.value);
-    let leftoverMoney = 50;//parseInt(leftover.value);
-    checkNum(selectedNumber);
-    counterMoney(leftoverMoney, money, selectedNumber);
-    
-}
-//check the selected num with the random
-function checkNum(selectedNumber){
-    if (selectedNumber === randomNumber){
-        playText.innerHTML=winningMessage;
-    }else { 
-        playText.innerHTML=losingMessage
-    }
-}
-
-function counterMoney(leftOverMoney, money, selectedNumber){
-    let totalMoney = leftOverMoney;
-    if(selectedNumber === randomNumber){
-        totalMoney = totalMoney + (inputNum*2);
-    }else { 
-        totalMoney = totalMoney - money;
-    }
-
-    leftover.innerHTML="Saldo: " + totalMoney;
+    let selectedNumber=parseInt(selectNumber.value);
 }
